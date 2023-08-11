039e6a08d18af93d647c67a40449000298984e19
furqan41 <105802251+furqan41@users.noreply.github.com>
2023-08-11 21:46:15 +0000

    Add files via upload

---
+++ SOFAR_Object_Following_Robot/Navigation_2/nav2_bringup/nav2_gazebo_spawner/nav2_gazebo_spawner/nav2_simulation.py
@@ -0,0 +1,47 @@
+from geometry_msgs.msg import PoseStamped
+from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
+import rclpy
+from rclpy.duration import Duration
+
+def main():
+    rclpy.init()
+
+    navigator = BasicNavigator()
+
+
+   #set our demo's goal poses to follow
+    goal_poses = []
+    goal_pose1 = PoseStamped()
+    goal_pose1.header.frame_id = 'map'
+    goal_pose1.header.stamp = navigator.get_clock().now().to_msg()
+    goal_pose1.pose.position.x = 1.5
+    goal_pose1.pose.position.y = 0.55
+    goal_pose1.pose.orientation.w = 0.707
+    goal_pose1.pose.orientation.z = 0.707
+    goal_poses.append(goal_pose1)
+    
+    
+    
+    nav_start = navigator.get_clock().now()
+    navigator.followWaypoints(goal_poses)
+    
+    
+     # Do something depending on the return code
+    result = navigator.getResult()
+    if result == TaskResult.SUCCEEDED:
+        print('Goal succeeded!')
+    elif result == TaskResult.CANCELED:
+        print('Goal was canceled!')
+    elif result == TaskResult.FAILED:
+        print('Goal failed!')
+    else:
+        print('Goal has an invalid return status!')
+
+    navigator.lifecycleShutdown()
+
+    exit(0)
+
+
+if __name__ == '__main__':
+    main()
+
